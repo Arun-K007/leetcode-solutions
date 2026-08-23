c1a24d0c4eba3f0d78f073d6e789e08dd389d7eb
Arun .Krishnamoorthy <arun_23aia11@kgkite.ac.in>
2026-08-23 14:00:11 +0530

    Time: 6 ms (97.08%), Space: 47.3 MB (10.95%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean sumGame(String num) {
+
+        int n = num.length();
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        // First half
+        for (int i = 0; i < n / 2; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                leftQ++;
+            } else {
+                leftSum += c - '0';
+            }
+        }
+
+        // Second half
+        for (int i = n / 2; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                rightQ++;
+            } else {
+                rightSum += c - '0';
+            }
+        }
+
+        // Odd number of '?' -> Alice always wins
+        if ((leftQ + rightQ) % 2 == 1) {
+            return true;
+        }
+
+        // Bob can win only if this exact balance is possible
+        if (leftSum - rightSum == 9 * (rightQ - leftQ) / 2) {
+            return false;
+        }
+
+        return true;
+    }
+}
