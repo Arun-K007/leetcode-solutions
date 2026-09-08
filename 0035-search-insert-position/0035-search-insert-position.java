cde72c981d4848c7b84a4103a67218ffaa1c93a3
Arun .Krishnamoorthy <arun_23aia11@kgkite.ac.in>
2026-09-08 18:28:40 +0530

    Time: 0 ms (100%), Space: 44.6 MB (66.68%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int count =0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]<target){
+                count=i+1;
+            }
+            if(nums[i]==target){
+                return count;
+            }
+        }
+        return count ;
+        
+    }
+}
