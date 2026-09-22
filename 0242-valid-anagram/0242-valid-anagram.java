9afde0decc8018e44364ce450c8305c09f18df83
Arun .Krishnamoorthy <arun_23aia11@kgkite.ac.in>
2026-09-22 09:47:54 +0530

    Time: 2 ms (99.62%), Space: 44.6 MB (55.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] arr=new int[26];
-        for( char x:s.toCharArray()){
+        int[] arr = new int[26];
+        for (int x:s.toCharArray()){
             arr[x-'a']++;
         }
-        for(char x:t.toCharArray()){
+        for(int x:t.toCharArray()){
             arr[x-'a']--;
         }
         for(int i=0;i<arr.length;i++){
