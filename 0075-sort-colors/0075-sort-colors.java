19f698c30288151c257bf3d66a06ce33a9c1ca9e
Arun .Krishnamoorthy <arun_23aia11@kgkite.ac.in>
2026-09-23 12:34:48 +0530

    Time: 70 ms (1.9%), Space: 43.7 MB (7.47%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] count= new int[3];
+        for(int x:nums){
+            if(x==0){
+                count[0]++;
+            }
+            else if(x==1){
+                count[1]++;
+            }
+            else{
+                count[2]++;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(count[0]>0){
+                nums[i]=0;
+                count[0]--;
+            }
+            else if(count[1]>0){
+                nums[i]=1;
+                count[1]--;
+            }
+            else{
+                nums[i]=2;
+                count[2]--;
+            }
+        }
+    }
+}
